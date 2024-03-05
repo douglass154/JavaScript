3b1392d3756bc6031906246ea53169ef7ffbd4fd
Douglas Silva <158169029+douglass154@users.noreply.github.com>
2024-03-04 23:12:10 -0300

    att título HTML

--- curso-em-video/desafios/desafio01/horario-flexivel/script.js
+++ curso-em-video/desafios/desafio01/horario-flexivel/script.js
@@ -9,22 +9,22 @@ function load() {
 
     if (hours >= 1 && hours < 5) {
         mensagem.innerHTML = `Agora são <strong>${hours}:${minutes}</strong> da madrugada.`
-        document.getElementsByTagName('body')[0].style.backgroundColor = '#0ea7b5'
+        document.body.style.backgroundColor = '#0ea7b5'
         foto.innerHTML = "<img src='imagens/night.png'>"
         document.getElementById('title').style.color = '#fff'
         document.getElementById('direitos').style.color = '#fff'
     }
     else if (hours > 5 && hours < 12) {
         mensagem.innerHTML = `Agora são <strong>${hours}:${minutes}</strong> da manhã.`
-        document.getElementsByTagName('body')[0].style.backgroundColor = '#6bd2db'
+        document.body.style.backgroundColor = '#6bd2db'
         foto.innerHTML = "<img src='imagens/morning.png'>"
     } else if (hours >= 12 && hours < 18) {
         mensagem.innerHTML = `Agora são <strong>${hours}:${minutes}</strong> da tarde.`
-        document.getElementsByTagName('body')[0].style.backgroundColor = '#e8702a'
+        document.body.style.backgroundColor = '#e8702a'
         foto.innerHTML = "<img src='imagens/afternoon.png'>"
     } else {
         mensagem.innerHTML = `Agora são <strong>${hours}:${minutes}</strong> da noite.`
-        document.getElementsByTagName('body')[0].style.backgroundColor = '#0c457d'
+        document.body.style.backgroundColor = '#0c457d'
         foto.innerHTML = "<img src='imagens/night.png'>"
         document.getElementById('title').style.color = '#fff'
         document.getElementById('direitos').style.color = '#fff'
